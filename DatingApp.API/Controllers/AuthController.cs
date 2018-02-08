5a0ccc485365a4d85fd7a0286feade41cf004334
KumarDeekonda <kumardvplr@gmail.com>
2018-02-08 00:49:05 -0500

    Added  Authentication Middleware

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -8,37 +8,41 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using System;
+using Microsoft.Extensions.Configuration;
 
 namespace DatingApp.API.Controllers
-{ 
+{
     [Route("api/[controller]")]
     public class AuthController : Controller
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
-         _repo = repo;
+            _config = config;
+            _repo = repo;
 
         }
         [HttpPost("register")]
 
         public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForregisterDto)
         {
-             userForregisterDto.Username = userForregisterDto.Username.ToLower();
+            userForregisterDto.Username = userForregisterDto.Username.ToLower();
 
-             if(await _repo.UserExists(userForregisterDto.Username))
-                  //return BadRequest("Username is already taken");
-                  ModelState.AddModelError("Username", "Username is already exists!!");
+            if (await _repo.UserExists(userForregisterDto.Username))
+                //return BadRequest("Username is already taken");
+                ModelState.AddModelError("Username", "Username is already exists!!");
 
             // validate the request
-             if(!ModelState.IsValid)
-                 return BadRequest(ModelState);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-            var userToCreate = new User {
+            var userToCreate = new User
+            {
                 Username = userForregisterDto.Username
             };
 
-            var createUser = await _repo.Register(userToCreate,userForregisterDto.Password);
+            var createUser = await _repo.Register(userToCreate, userForregisterDto.Password);
 
             return StatusCode(201);
         }
@@ -47,29 +51,30 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
         {
-             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
-             
-              if(userFromRepo == null)
-                   return Unauthorized();
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
 
             //generating JSON Web Token 
 
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes("SuperSecret");
-            var tokenDescriptor= new SecurityTokenDescriptor{
-                Subject = new ClaimsIdentity( new Claim[] {
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
                     new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
                     new Claim(ClaimTypes.Name, userFromRepo.Username)
                 }),
                 Expires = DateTime.Now.AddDays(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), 
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
                                                SecurityAlgorithms.HmacSha512Signature)
             };
-            
+
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var tokenString = tokenHandler.WriteToken(token);
 
-            return Ok( new { tokenString });
+            return Ok(new { tokenString });
         }
     }
 }
