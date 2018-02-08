d322e5a00cb762eeb9b48d9186c13ca128c1ae8b
KumarDeekonda <kumardvplr@gmail.com>
2018-02-07 23:23:43 -0500

    UserLogin Api created with JSON WEB Token

---
+++ DatingApp.API/Data Transfer Objects/UserForLoginDto.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Data_Transfer_Objects
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
