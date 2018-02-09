d4a55927d15eb2596f5f89e30d8de38ddf893dc7
KumarDeekonda <kumardvplr@gmail.com>
2018-02-08 23:45:10 -0500

    Implemented Authservice and Validated User, stored token at localstorage

---
+++ DatingApp.SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+   model: any = { };
+  constructor(private _authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this._authService.login(this.model).subscribe(data => {
+      console.log('logged in successfully');
+        });
+  }
+}
