15703d75214a4e2d96bb4a1839f9e41571631579
KumarDeekonda <kumardvplr@gmail.com>
2018-02-10 01:45:21 -0500

    Handling Errors With Global  Exception at Server Side

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -51,6 +51,8 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
         {
+            throw new Exception("Computer Says No!!");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
