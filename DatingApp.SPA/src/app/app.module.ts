392724d590b048878a3ed2912ed8bebd16d52052
KumarDeekonda <kumardvplr@gmail.com>
2018-02-11 02:13:21 -0500

    Ngx bootsrap to power up our components

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AlertifyService } from './_services/alertify.service';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { AlertifyService } from './_services/alertify.service';
+
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpModule, FormsModule],
+  imports: [BrowserModule, HttpModule, FormsModule, BsDropdownModule.forRoot()],
   providers: [AuthService, AlertifyService],
   bootstrap: [AppComponent]
 })
