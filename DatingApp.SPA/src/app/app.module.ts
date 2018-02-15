3e4c3fc2bccbc83aa3a88ca3d2e0ea33a795f576
KumarDeekonda <kumardvplr@gmail.com>
2018-02-14 22:58:58 -0500

    Added Member Details View

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AlertifyService } from './_services/alertify.service';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
 import { AuthService } from './_services/auth.service';
@@ -19,17 +19,29 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
-
-
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent,
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
     MemberListComponent,
     ListsComponent,
     MessagesComponent,
-    MemberCardComponent
-],
-  imports: [BrowserModule, HttpModule, FormsModule, BsDropdownModule.forRoot(), RouterModule.forRoot(appRoutes), AuthModule],
+    MemberCardComponent,
+    MemberDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    AuthModule,
+    TabsModule.forRoot()
+  ],
   providers: [AuthService, AlertifyService, AuthGuard, UserService],
   bootstrap: [AppComponent]
 })
